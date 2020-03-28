411372c7a566b7484e11426c9024587ead9babd2
Bukiatorrr <bukinator0272@gmail.com>
2020-03-28 19:51:15 +0400

    Some DB-filling operations

--- src/main/resources/DB.sql
+++ src/main/resources/DB.sql
@@ -28,22 +28,22 @@ CREATE TABLE employee
     FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
 
-INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO department (name) VALUES ('NetСracker');
 INSERT INTO position (name) VALUES ('Director');
 INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2010-01-01', 'Grigoriy', 'Fligil', 1, 1, null);
 
-INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO department (name) VALUES ('NetСracker');
 INSERT INTO position (name) VALUES ('Manager');
 INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2018-04-3', 'Lena', 'Chernova', 2, 2, 1);
 
-INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO department (name) VALUES ('NetСracker');
 INSERT INTO position (name) VALUES ('TeamLead');
 INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Nikita', 'Burenkov', 3, 3, 2);
 
-INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO department (name) VALUES ('NetСracker');
 INSERT INTO position (name) VALUES ('Developer');
 INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Liza', 'Ganina', 4, 4, 3);
 
-INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO department (name) VALUES ('NetСracker');
 INSERT INTO position (name) VALUES ('Junior');
 INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Dima', 'Bukin', 5, 5, 4);
