65f9249b7e086875ecebd5e7afa0bd0eaa8ffb21
Bukiatorrr <bukinator0272@gmail.com>
2020-03-28 19:28:19 +0400

    Some DB-filling operations

--- src/main/resources/DB.sql
+++ src/main/resources/DB.sql
@@ -2,6 +2,18 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS position;
 
+CREATE TABLE department
+(
+    department_id serial PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE position
+(
+    position_id serial PRIMARY KEY,
+    name VARCHAR(255)
+);
+
 CREATE TABLE employee
 (
     employee_id serial PRIMARY KEY,
@@ -16,14 +28,22 @@ CREATE TABLE employee
     FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
 
-CREATE TABLE department
-(
-    department_id serial PRIMARY KEY,
-    name VARCHAR(255)
-);
+INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO position (name) VALUES ('Director');
+INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2010-01-01', 'Grigoriy', 'Fligil', 1, 1, null);
 
-CREATE TABLE position
-(
-    position_id serial PRIMARY KEY,
-    name VARCHAR(255)
-);
+INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO position (name) VALUES ('Manager');
+INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2018-04-3', 'Lena', 'Chernova', 2, 2, 1);
+
+INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO position (name) VALUES ('TeamLead');
+INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Nikita', 'Burenkov', 3, 3, 2);
+
+INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO position (name) VALUES ('Developer');
+INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Liza', 'Ganina', 4, 4, 3);
+
+INSERT INTO department (name) VALUES ('Cracker');
+INSERT INTO position (name) VALUES ('Junior');
+INSERT INTO employee (employment_date, name, surname, department_id, position_id, manager_id) VALUES ('2020-03-27', 'Dima', 'Bukin', 5, 5, 4);
